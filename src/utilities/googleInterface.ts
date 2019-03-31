274d49fdf66fc7bdfea95f9685b94b072c2444a9
Ted Shaffer <ted@brightsign.biz>
2019-03-31 07:12:14 -0700

    improved Promise management.

--- src/utilities/googleInterface.ts
+++ src/utilities/googleInterface.ts
@@ -14,46 +14,46 @@ export function getGoogleAlbums(accessToken: string): Promise<GoogleAlbum[]> {
   let allGoogleAlbums: GoogleAlbum[] = [];
   const apiEndpoint = 'https://photoslibrary.googleapis.com';
 
-  return new Promise((resolve, reject) => {
+  const processGetAlbums = (pageToken: string): Promise<GoogleAlbum[]> => {
 
-    const processGetAlbums = (pageToken: string) => {
-      let url = apiEndpoint + '/v1/albums?pageSize=50';
-      if (pageToken !== '') {
-        url = url + '&pageToken=' + pageToken;
+    let url = apiEndpoint + '/v1/albums?pageSize=50';
+    if (pageToken !== '') {
+      url = url + '&pageToken=' + pageToken;
+    }
+
+    const AuthStr = 'Bearer '.concat(accessToken);
+    return axios.get(
+      url,
+      {
+        headers: { Authorization: AuthStr },
       }
-
-      const AuthStr = 'Bearer '.concat(accessToken);
-      axios.get(
-        url,
-        {
-          headers: { Authorization: AuthStr },
-        }
-      )
-        .then((axiosResult: any) => {
-          const result: any = axiosResult.data;
-          const googleAlbums: GoogleAlbum[] = result.albums.map((downloadedGoogleAlbum: any) => {
-            return {
-              googleAlbumId: downloadedGoogleAlbum.id,
-              title: downloadedGoogleAlbum.title,
-              productUrl: downloadedGoogleAlbum.productUrl,
-              mediaItemsCount: downloadedGoogleAlbum.mediaItemsCount,
-              mediaItemIds: [],
-            } as GoogleAlbum;
-          });
-
-          allGoogleAlbums = allGoogleAlbums.concat(googleAlbums);
-
-          if (result.albums.length === 0 || result.nextPageToken === undefined) {
-            resolve(allGoogleAlbums);
-          }
-          else {
-            processGetAlbums(result.nextPageToken);
-          }
-        }).catch( (err: any) => {
-          debugger;
+    )
+      .then((axiosResult: any) => {
+        const result: any = axiosResult.data;
+        const googleAlbums: GoogleAlbum[] = result.albums.map((downloadedGoogleAlbum: any) => {
+          return {
+            googleAlbumId: downloadedGoogleAlbum.id,
+            title: downloadedGoogleAlbum.title,
+            productUrl: downloadedGoogleAlbum.productUrl,
+            mediaItemsCount: downloadedGoogleAlbum.mediaItemsCount,
+            mediaItemIds: [],
+          } as GoogleAlbum;
         });
-    };
-    processGetAlbums('');
-  });
+
+        allGoogleAlbums = allGoogleAlbums.concat(googleAlbums);
+
+        if (result.albums.length === 0 || result.nextPageToken === undefined) {
+          return Promise.resolve(allGoogleAlbums);
+        }
+        else {
+          return processGetAlbums(result.nextPageToken);
+        }
+      }).catch((err: any) => {
+        debugger;
+        return Promise.resolve(allGoogleAlbums);
+      });
+  };
+
+  return processGetAlbums('');
 }
 
